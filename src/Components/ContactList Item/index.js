6a0c56b22e9a3c0665a8ca81afcedceaf5998b6a
Bryan Ansong <50881304+bryanansong@users.noreply.github.com>
2023-07-04 16:00:33 -0400

    Backend-5 ChatRooms

--- src/Components/ContactList Item/index.js
+++ src/Components/ContactList Item/index.js
@@ -1,14 +1,48 @@
 import { Text, View, Image, StyleSheet, Pressable } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime'
+import relativeTime from 'dayjs/plugin/relativeTime';
+import { API, graphqlOperation, Auth } from 'aws-amplify';
+import { createChatRoom, createUserChatRoom } from '../../graphql/mutations';
+import { getCommonChatRoomWithUser } from '../../Services/chatRoomService';
+
 dayjs.extend(relativeTime);
 
 const ContactListItem = ({ user }) => {
     const navigation = useNavigation();
 
+    const onPress = async () => {
+        // Check if we already have a ChatRoom with user
+        const existingChatRoom  = await getCommonChatRoomWithUser(user.id)
+
+        if (existingChatRoom) {
+            navigation.navigate("Chat", {id: existingChatRoom.id })
+            return;
+        }
+
+        // Create a new chatroom
+        const newChatRoomData = await API.graphql(graphqlOperation(createChatRoom, { input: {}}));
+        console.log(newChatRoomData);
+
+        if (!newChatRoomData.data?.createChatRoom) {
+            console.log("Error creating chat room")
+        }
+
+        const newChatRoom = newChatRoomData.data?.createChatRoom;
+
+        // Add the clicked user to the chatroom
+        await API.graphql(graphqlOperation(createUserChatRoom, { input: {chatRoomID: newChatRoom.id, userID: user.id}}))
+
+        // Add the auth user to the chat room
+        const authUser = await Auth.currentAuthenticatedUser();
+        await API.graphql(graphqlOperation(createUserChatRoom, { input: {chatRoomID: newChatRoom.id, userID: authUser.attributes.sub }}))
+
+        // navigate to the newly created chat room
+        navigation.navigate("Chat", { id: newChatRoom.id })
+    }
+
     return (
-        <Pressable onPress={() => {}} style={styles.container}>
+        <Pressable onPress={ onPress } style={styles.container}>
             <Image source={{uri: user.image}} style={styles.image}/>
             <View style={styles.content}>
                 <Text numberOfLines={1} style={styles.name}>
