1872d36c70f1a4006ba90b5e6e84e0d7f65c8ddd
Bryan Ansong <50881304+bryanansong@users.noreply.github.com>
2023-07-05 18:13:37 -0400

    Update queries.js

--- src/screens/ChatsScreen/queries.js
+++ src/screens/ChatsScreen/queries.js
@@ -1,30 +1,28 @@
 export const listChatRooms = /* GraphQL */ `
-    query GetUser($id: ID!) {
-        getUser(id: $id) {
-        id
-        ChatRooms {
-            items {
+  query GetUser($id: ID!) {
+    getUser(id: $id) {
+      id
+      ChatRooms {
+        items {
+          chatRoom {
             id
-            chatRoom {
-                id
-                users {
-                items {
-                    id
-                    user {
-                    id
-                    image
-                    name
-                    }
+            users {
+              items {
+                user {
+                  id
+                  image
+                  name
                 }
-                }
-                LastMessage {
-                id
-                createdAt
-                text
-                }
+              }
             }
+            LastMessage {
+              id
+              createdAt
+              text
             }
+          }
         }
-        }
+      }
     }
-  `;
+  }
+`;
